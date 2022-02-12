23c7c623571660961ef5c5401f58b898afbfe2ea
WarrenN <noronhawarren@hotmail.com>
2022-02-12 13:31:38 +1300

    inbox-updated

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+    'sad tape please duck panic ten stable any manual humble ready plug', 'https://rinkeby.infura.io/v3/e03ad84e1be04a3999d4b619aeeb0ebc'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+};
+
+deploy();
